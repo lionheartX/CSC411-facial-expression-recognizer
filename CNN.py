76cf1419a66b9a3c72b41fd439b5d2e92fe757a8
lionheartX <lionheart.xiong@gmail.com>
2016-04-08 00:11:11 -0400

    Place to put our references

--- CNN.py
+++ CNN.py
@@ -1,30 +1,39 @@
 #Hyperparameters
-TRAIN_TEST_SPLIT_RATIO = 0.2
+TRAIN_TEST_SPLIT_RATIOS = [0.1, 0.15, 0.2]
+ACTIVATION_FUNCTIONS = ["Rectifier", "", "Sigmoid"]
+CHANNELS = [4, 8]
+KERNEL_SHAPES=[(2,2),(3,3),(4,4),(16,1),(1,16)]
+LAYERS = ["Softmax", ]
 
+# dropout leasanier, GTX 970
+# MNIST layer
 
 def loadData():
 	data = sio.loadmat('./labeled_images.mat')
 	X = data['tr_images'].T
-	#X = X.reshape((-1, 1024)).astype('float');
-	#X *= (1.0/X.max())
+	X = X.astype('float')
+	X *= (1.0/X.max())
 	print X.shape
 	y = data['tr_labels'][:, 0]
 	return X, y
 
 def CNN(X, y):
-	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=TRAIN_TEST_SPLIT_RATIO)
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2, random_state = 42)
 	nn = Classifier(
     layers=[
-        Convolution("Rectifier", channels=8, kernel_shape=(3,3)),
-        Layer("Softmax")], learning_rate=0.02, n_iter=5)
+        Convolution("Rectifier", channels=7, kernel_shape=(3,3)),
+        Layer("Softmax")], learning_rate=0.02, n_iter=10)
 	nn.fit(X_train, y_train)
 	print('\nTRAIN SCORE', nn.score(X_train, y_train))
 	print('TEST SCORE', nn.score(X_test, y_test))
 	#y_pred = nn.predict(X_test)
 
+def batch_run():
+	pass
+
 def main():
-	X_train, y_train = loadData()
-	CNN(X_train, y_train)
+	X, y = loadData()
+	CNN(X, y)
 
 if __name__ == '__main__':
 	print "Import libaries..."
@@ -35,11 +44,11 @@ if __name__ == '__main__':
 	from sknn.mlp import Classifier, Convolution, Layer
 	import logging
 	logging.basicConfig()
-	digits = datasets.load_digits()
-	X = digits.images
-	y = digits.target
-	CNN(X, y)
-	print X
-	print X.shape
+	#digits = datasets.load_digits()
+	#X = digits.images
+	#y = digits.target
+	#CNN(X, y)
+	#print X
+	#print X.shape
 	print("-------------------------")
-	#main()
+	main()
